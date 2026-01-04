695008f5386b1b53c9cc8008af99cddf41a4b199
Bumbleeeee <78940935+Bumbleeeee@users.noreply.github.com>
2026-01-04 10:25:52 -0800

    multidimensional framework and more instructions
    - created gradient descent method
    - functions to handle step size (constant and based on iteration number, still need to implement optimal step size)
    - the user can now choose how many iterations they would like to perform
    - added more info to README.md, explaining some nuances

--- README.md
+++ README.md
@@ -1,6 +1,20 @@
 # Optimization-calculator
 A calculator for various numerical optimization algorithms
 
-run the following line to install the required packages:
+### Installation
+Run the following line to install the required packages:
 
-pip install -r requirements.txt
+     pip install -r requirements.txt 
+
+
+### Important input information
+
+- Variable names must be written **exactly** as stated, or the calculator will treat them as constants.
+
+- For multidimensional algorithms (R<sup>n</sup> → R), n is determined by the number of elements in the starting point you provide
+
+- When using x, y, z as the variables, you must use them in the written order. For example, a function from R<sup>2</sup> → R must be a function f(x, y).
+
+- x = x_1, y = x_2, z = x_3, so using the two variable naming conventions in the same equation is generally not advised. For example, x + x_1 = 2*x_1. **Note how this is an expression of one variable, not two**.
+
+- When entering a step size function, **note the iteration number starts at 1**. Ensure the function is defined for all integers {1,...,k}, where k is the number of iterations. Otherwise, the program will crash.
---
+++ multidimAlgs.py
@@ -0,0 +1,77 @@
+import sympy
+x, y, z = sympy.symbols('x y z')
+# note the use of point-direction-stepsize so individual iterations should probably handle the step size
+# the method func probably only handles the type
+#       (just pass through to iter which delegates the step size work elsewhere right before returning)
+# user should be able to enter a number, function, OR choose optimal step size
+# WHEN ENTERING A FUNCTION NOTE THE ITER NUM STARTS AT 1 -- need to make this clear
+
+
+
+def findStepSize(func, stepSizeFunc, iterNum):
+    if stepSizeFunc == sympy.oo:
+        #TODO: optimal step size
+        return 0.25
+    else:
+        return stepSizeFunc.evalf(subs={x : iterNum})
+
+
+def gradientIteration(expr, point, iterNum, varList, stepSizeFunc):
+    variables = sympy.Matrix(varList)
+    subsDict = {}
+    for i in range(len(variables)):
+        subsDict[variables[i]] = point[i]
+
+    gradient = sympy.Matrix([expr]).jacobian(variables).T
+    stepSizeNum = findStepSize(expr, stepSizeFunc, iterNum)
+    print(stepSizeNum)
+    return point - gradient.evalf(subs=subsDict) * stepSizeNum
+
+
+def gradientMethod(expr, point, stepSizeFunc, numIters = 10):
+    # create enough vars
+    x_1 = sympy.symbols('x_1')
+    varString = " ".join(f"x_{i+1}" for i in range(point.__len__()))
+    x_symbols = sympy.symbols(varString, seq=True)
+
+    # remove x,y,z if present
+    xyzList = [x, y, z]
+    for i in range(min(point.__len__(), 3)):
+        expr = expr.subs(xyzList[i], x_symbols[i])
+
+    # run iters
+    for i in range(numIters):
+        point = gradientIteration(expr, point, i+1, x_symbols, stepSizeFunc)
+    return point
+
+
+def getPoint():
+    while True:
+        try:
+            pointStr = input("Enter a starting point. For multiple dimensions, separate with a space: ")
+            pointListStr = pointStr.split()
+            point = []
+            for p in pointListStr:
+                point.append(float(p))
+            return point
+        except ValueError:
+            print("Please enter real numbers for each coordinate.")
+
+
+def getStepSizeInput():
+    while True:
+        stepSize = input("\nEnter step size. "
+                         "\nIt can be a constant, a function of 'x' - the iteration number (starting at 1), "
+                         "\nor type 'optimal' to use the optimal step size at each iteration: ")
+        if (stepSize.lower() == 'optimal'):
+            stepSize = sympy.oo
+            break
+        else:
+            stepSize = sympy.sympify(stepSize)
+            # eval at 1 should be safe since first iter must evaluate at 1
+            if (sympy.ask(sympy.Q.real(stepSize.evalf(subs={x: 1}))) == True):
+                break
+            else:
+                print("\nPlease enter a valid step size.")
+
+    return stepSize
